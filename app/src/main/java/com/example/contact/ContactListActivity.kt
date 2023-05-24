f8fa70a35d7be90728b05ba5b4ce08ad2dcb7195
Tomszy <tothtomi007@gmail.com>
2023-05-24 09:12:28 +0200

    Random contact from API

--- app/src/main/java/com/example/contact/ContactListActivity.kt
+++ app/src/main/java/com/example/contact/ContactListActivity.kt
@@ -2,19 +2,25 @@ package com.example.contact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.PrimaryKey
 import com.example.contact.databinding.ActivityContactListBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import java.io.InputStream
+import java.net.URL
 
 class ContactListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityContactListBinding
     private lateinit var contactDb : ContactDatabase
+    private var arrayOfJson = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,8 @@ class ContactListActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         contactDb = ContactDatabase.getDatabase(this)
+
+        //GlobalScope.launch(Dispatchers.IO) { contactDb.contactDao().deleteAllContact()}
 
         val adapter = ListAdapter()
         val recyclerView = binding.recyclerview
@@ -41,19 +49,32 @@ class ContactListActivity : AppCompatActivity() {
     }
 
     private fun addContact(){
-        val id = 0
-        val name = "Jimmy"
-        val location = "Debrecen"
-        val email = "jimmy@gmail.com"
-        val phone = "0630-1234567"
-        val cell = "0620-1234567"
-        val picture = "https://img.freepik.com/free-vector/businessman-get-idea_1133-350.jpg?"
-
-        val contact = Contact(id, name, location, email, phone, cell, picture)
 
         GlobalScope.launch(Dispatchers.IO) {
+            val apiResponse = URL("https://randomuser.me/api").readText()
+            println(apiResponse)
+            val contact = getDataFromResponse(apiResponse)
             contactDb.contactDao().addContact(contact)
-
         }
+    }
+
+    private fun getDataFromResponse(response: String): Contact{
+
+        val id = 0
+        val name = findWordInResponse(response,"title") + " " + findWordInResponse(response,"first") + " " + findWordInResponse(response,"last")
+        val location = findWordInResponse(response, "city") + ", " + findWordInResponse(response.substring(response.indexOf("name") + 4), "name") + " " + findWordInResponse(response, "numbe").substring(0,findWordInResponse(response, "numbe").length-1)
+        val email = findWordInResponse(response, "email")
+        val phone = findWordInResponse(response, "phone")
+        val cell = findWordInResponse(response, "cell")
+        val picture = findWordInResponse(response, "large")
+
+        return Contact(id,name, location, email,phone,cell,picture)
+    }
+
+    private fun findWordInResponse(input: String, word: String):String{
+        val index = input.indexOf(word)
+        val firstHalf = input.substring(index + word.length + 3)
+        val secondHalf = firstHalf.substring(0, firstHalf.indexOf("\""))
+        return secondHalf
     }
 }
